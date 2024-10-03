94fb85d0063bc7f36fef3be0cf374ddf44084eb7
Jamie Thomas <jamiewthomas13@gmail.com>
2024-10-03 21:17:14 +0100

    Switch telescope-fzf-native to build="make"

--- lua/jamie/plugins/telescope.lua
+++ lua/jamie/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"}
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = "make"}
     },
     config = function()
         local telescope = require("telescope")
