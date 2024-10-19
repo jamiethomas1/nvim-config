b2b68f766755c91b3f0a18eeb85e258ae7e75b0e
Jamie Thomas <jamiewthomas13@gmail.com>
2024-10-19 01:00:40 +0100

    Disable treesitter indent for HTML files

--- lua/jamie/plugins/nvim-treesitter.lua
+++ lua/jamie/plugins/nvim-treesitter.lua
@@ -13,7 +13,10 @@ return {
       ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "html", "css", "tsx", "markdown", "python", "bash", "dockerfile", "fish", "json", "scss" },
       sync_install = false,
       highlight = { enable = true },
-      indent = { enable = true },
+      indent = { 
+        enable = true, 
+        disable = { "html" }
+      },
     })
   end
 }
