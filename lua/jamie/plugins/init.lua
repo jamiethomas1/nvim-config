8b398b93b712aa6ce48d8f153ce4f2da38d44766
Jamie Thomas <jamiewthomas13@gmail.com>
2024-11-03 13:55:30 +0000

    Add copilot.nvim and vim-move

--- lua/jamie/plugins/init.lua
+++ lua/jamie/plugins/init.lua
@@ -2,5 +2,7 @@ return {
   "christoomey/vim-tmux-navigator",
   "voldikss/vim-floaterm",
   "tpope/vim-fugitive",
-  "bullets-vim/bullets.vim"
+  "bullets-vim/bullets.vim",
+  "github/copilot.vim",
+  "matze/vim-move",
 }
