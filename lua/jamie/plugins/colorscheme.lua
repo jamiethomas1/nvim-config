9007c9cf72b65cfedd78b8521b556ae0756cbe65
Jamie Thomas <jamiewthomas13@gmail.com>
2024-11-12 08:25:45 +0000

    Switch to catppuccin theme

--- lua/jamie/plugins/colorscheme.lua
+++ lua/jamie/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
-  "rebelot/kanagawa.nvim",
+  "catppuccin/nvim",
   priority = 1000, -- load before all other plugins, default for plugins is 50
   config = function ()
-    vim.cmd([[colorscheme kanagawa-dragon]])
+    vim.cmd([[colorscheme catppuccin-mocha]])
   end
 }
