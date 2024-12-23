5331d72043c4587156a2b341a14d56b7fc407c33
Jamie Thomas <jamiewthomas13@gmail.com>
2024-12-23 08:31:44 +0000

    Switch to rose-pine-moon colorscheme

--- lua/jamie/plugins/colorscheme.lua
+++ lua/jamie/plugins/colorscheme.lua
@@ -3,12 +3,13 @@ local utils = require("utils")
 -- Previous colorschemes: 
 -- "rebelot/kanagawa.nvim",
 -- "catppuccin/nvim",
+-- "rose-pine/neovim",
 
 return {
-  "catppuccin/nvim",
+  "rose-pine/neovim",
   enabled = not utils.is_vscode,
   priority = 1000, -- load before all other plugins, default for plugins is 50
   config = function ()
-    vim.cmd([[colorscheme catppuccin-mocha]])
+    vim.cmd([[colorscheme rose-pine-moon]])
   end
 }
