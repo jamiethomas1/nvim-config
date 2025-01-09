c7383fa03d72caf47faaa7c8a326e74f1893a397
Jamie Thomas <jamiewthomas13@gmail.com>
2025-01-09 08:03:25 +0000

    Modify colorscheme to have black bg

--- lua/jamie/plugins/colorscheme.lua
+++ lua/jamie/plugins/colorscheme.lua
@@ -10,6 +10,14 @@ return {
   enabled = not utils.is_vscode,
   priority = 1000, -- load before all other plugins, default for plugins is 50
   config = function ()
+    require("rose-pine").setup({
+      palette = {
+        moon = {
+          base = "#000000",
+          overlay = "#1a1820",
+        }
+      }
+    })
     vim.cmd([[colorscheme rose-pine-moon]])
   end
 }
