abaf4db8ec005c6994767a5faa7270849a7fa89d
Jamie Thomas <jamiewthomas13@gmail.com>
2025-01-29 23:48:05 +0000

    Remove horizontal movement keybinds from vim-move

--- lua/jamie/plugins/vim-move.lua
+++ lua/jamie/plugins/vim-move.lua
@@ -3,5 +3,8 @@ return {
   config = function()
     vim.g.move_left_key = ""
     vim.g.move_right_key = ""
+
+    vim.api.nvim_del_keymap("n", "<A-h>")
+    vim.api.nvim_del_keymap("n", "<A-l>")
   end
 }
