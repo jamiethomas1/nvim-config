365ac03ee0b773c5d9efc2d63484e909f43554e2
Jamie Thomas <jamiewthomas13@gmail.com>
2025-03-30 23:20:51 +0100

    fix: Fix text de-indenting when adding Go marshal tags

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -15,7 +15,18 @@ vim.keymap.set(
 vim.keymap.set(
   "n",
   "<leader>jt",
-  "A `json:\"\"`<Esc>F\"i",
+  function()
+    local line = vim.api.nvim_get_current_line()
+    local cursor = vim.api.nvim_win_get_cursor(0)
+
+    -- Append the `json:""` text at the end of the line
+    line = line .. ' `json:""`'
+    vim.api.nvim_set_current_line(line)
+
+    -- Move the cursor to the correct position inside the quotes
+    vim.api.nvim_win_set_cursor(0, { cursor[1], #line - 2 })
+    vim.cmd("startinsert")
+  end,
   { buffer = true }
 )
 
