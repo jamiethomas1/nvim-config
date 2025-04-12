3b3d1487d8d5df7a94b14c69b88fe7292b6a6f2b
Jamie Thomas <jamiewthomas13@gmail.com>
2025-04-12 22:07:47 +0100

    feat: Improve modularity of colour scheme selection

--- lua/jamie/plugins/colorscheme.lua
+++ lua/jamie/plugins/colorscheme.lua
@@ -5,28 +5,69 @@ local utils = require("utils")
 -- "catppuccin/nvim",
 -- "rose-pine/neovim",
 
-return {
-  "rose-pine/neovim",
-  enabled = not utils.is_vscode,
-  priority = 1000, -- load before all other plugins, default for plugins is 50
-  config = function ()
-    require("rose-pine").setup({
-      palette = {
-        moon = {
-          base = "#000000",
-          overlay = "#1a1820",
+local colorschemes = {
+  {
+    "rose-pine/neovim",
+    enabled = not utils.is_vscode,
+    priority = 1000, -- load before all other plugins, default for plugins is 50
+    config = function ()
+      require("rose-pine").setup({
+        palette = {
+          moon = {
+            base = "#000000",
+            overlay = "#1a1820",
+          }
+        },
+        highlight_groups = {
+          TelescopeBorder = { fg = "highlight_high", bg = "none" },
+          TelescopeNormal = { bg = "none" },
+          TelescopePromptNormal = { bg = "base" },
+          TelescopeResultsNormal = { fg = "subtle", bg = "base" },
+          TelescopeSelection = { fg = "text", bg = "base" },
+          NormalFloat = { bg = "base" },
+          FloatBorder = { fg = "highlight_high", bg = "none" },
         }
-      },
-      highlight_groups = {
-        TelescopeBorder = { fg = "highlight_high", bg = "none" },
-        TelescopeNormal = { bg = "none" },
-        TelescopePromptNormal = { bg = "base" },
-        TelescopeResultsNormal = { fg = "subtle", bg = "base" },
-        TelescopeSelection = { fg = "text", bg = "base" },
-        NormalFloat = { bg = "base" },
-        FloatBorder = { fg = "highlight_high", bg = "none" },
-      }
-    })
-    vim.cmd([[colorscheme rose-pine-moon]])
-  end
+      })
+      vim.cmd([[colorscheme rose-pine-moon]])
+    end
+  },
+  {
+    "catppuccin/nvim",
+    enabled = not utils.is_vscode,
+    priority = 1000, -- load before all other plugins, default for plugins is 50
+    config = function ()
+      require("catppuccin").setup({
+        color_overrides = {
+          all = {
+            base = "#000000",
+            mantle = "#000000",
+            crust = "#000000",
+          },
+        },
+      })
+      vim.cmd([[colorscheme catppuccin-mocha]])
+    end
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    enabled = not utils.is_vscode,
+    priority = 1000, -- load before all other plugins, default for plugins is 50
+    config = function ()
+      require("kanagawa").setup({
+        transparent = true,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        }
+      })
+      vim.cmd([[colorscheme kanagawa-wave]])
+    end
+  },
 }
+
+return colorschemes[1]
