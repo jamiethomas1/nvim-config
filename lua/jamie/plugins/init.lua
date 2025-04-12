8c7617a6f6da3e6061a6a8c6109350e0b228ee9d
Jamie Thomas <jamiewthomas13@gmail.com>
2025-04-12 09:58:44 +0100

    feat: Re-install copilot

--- lua/jamie/plugins/init.lua
+++ lua/jamie/plugins/init.lua
@@ -4,7 +4,7 @@ if not utils.is_vscode then
   return {
     "christoomey/vim-tmux-navigator",
     "bullets-vim/bullets.vim",
-    -- "github/copilot.vim",
+    "github/copilot.vim",
     "JoosepAlviste/nvim-ts-context-commentstring",
   }
 end
