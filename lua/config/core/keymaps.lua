1bde5a882fb084dfc1b19d071d29d48878d9ccd3
Jamie Thomas <jamiewthomas13@gmail.com>
2026-01-01 14:06:02 +0000

    add lsp keybinds

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -48,4 +48,45 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "=ap", "ma=ap'a")
 
 -- lsp
-vim.keymap.set("n", "gd", "<C-u>zz", { noremap = true, silent = true, desc = "Scroll half page down & center cursor" })
+
+---@type vim.keymap.set.Opts
+local opts = {}
+
+opts.desc = "Show LSP references"
+vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
+opts.desc = "Go to declaration"
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+opts.desc = "Show LSP definitions"
+vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+opts.desc = "Show LSP implementations"
+vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+opts.desc = "Show LSP type definitions"
+vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+opts.desc = "See available code actions"
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+opts.desc = "Smart rename"
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+opts.desc = "Show buffer diagnostics"
+vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+opts.desc = "Show line diagnostics"
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+opts.desc = "Go to previous diagnostic"
+vim.keymap.set("n", "[d", vim.diagnostic.get_prev, opts) -- jump to previous diagnostic in buffer
+
+opts.desc = "Go to next diagnostic"
+vim.keymap.set("n", "]d", vim.diagnostic.get_next, opts) -- jump to next diagnostic in buffer
+
+opts.desc = "Show documentation for what is under cursor"
+vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+opts.desc = "Restart LSP"
+vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
