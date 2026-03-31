96fe9b689d0a6fab36823a8966734d22dd507495
Jamie Thomas <jamiewthomas13@gmail.com>
2026-03-31 20:08:13 +0100

    fix: Revert to old treesitter config for 0.11

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,46 @@
+-- return {
+--   'nvim-treesitter/nvim-treesitter',
+--   lazy = false,
+--   build = ':TSUpdate',
+--   event = { 'BufRead', 'BufNewFile' },
+--   install = {
+--     "javascript",
+--     "typescript",
+--     "jsdoc",
+--     "tsx",
+--     "lua",
+--     "bash",
+--     "go",
+--   }
+-- }
+
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
   lazy = false,
   build = ':TSUpdate',
-  install = {
-    "javascript",
-    "typescript",
-    "jsdoc",
-    "tsx",
-    "lua",
-  }
+  event = { 'BufRead', 'BufNewFile' },
+  config = function ()
+      local configs = require'nvim-treesitter.configs'
+
+      configs.setup({
+          ensure_installed = {
+              "javascript",
+              "typescript",
+              "jsdoc",
+              "tsx",
+              "lua",
+              "bash",
+              "go",
+          },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = {
+              enable = true,
+          },
+          modules = {},
+          auto_install = true,
+          ignore_install = {},
+      })
+  end
 }
