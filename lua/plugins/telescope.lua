496973d2ce444b881f8d3782ba0f8d9296c655a7
Jamie Thomas <jamiewthomas13@gmail.com>
2026-03-31 19:58:30 +0100

    feat: Add alias keybind ctrl+p for opening telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -58,6 +58,7 @@ return {
     -- set keymaps
 
     vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     vim.keymap.set("n", "<leader>fw", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
