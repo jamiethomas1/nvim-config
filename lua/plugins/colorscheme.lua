b9d4e7efa470e87ad6b242a5947a38ad31fa6c2d
Jamie Thomas <jamiewthomas13@gmail.com>
2026-03-31 20:09:40 +0100

    feat: Add catppuccin theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,54 @@
 local colorschemes = {
+    -- {
+    --     "rose-pine/neovim",
+    --     enabled = true,
+    --     priority = 1000, -- load before all other plugins, default for plugins is 50
+    --     config = function ()
+    --         require("rose-pine").setup({
+    --             palette = {
+    --                 moon = {
+    --                     base = "#000000",
+    --                     overlay = "#1a1820",
+    --                 }
+    --             },
+    --             highlight_groups = {
+    --                 TelescopeBorder = { fg = "highlight_high", bg = "none" },
+    --                 TelescopeNormal = { bg = "none" },
+    --                 TelescopePromptNormal = { bg = "base" },
+    --                 TelescopeResultsNormal = { fg = "subtle", bg = "base" },
+    --                 TelescopeSelection = { fg = "text", bg = "base" },
+    --                 NormalFloat = { bg = "base" },
+    --                 FloatBorder = { fg = "highlight_high", bg = "none" },
+    --             }
+    --         })
+    --         vim.cmd([[colorscheme rose-pine-moon]])
+    --     end
+    -- },
     {
-        "rose-pine/neovim",
+        "catppuccin/nvim",
         enabled = true,
-        priority = 1000, -- load before all other plugins, default for plugins is 50
+        priority = 1000,
+        name = "catppuccin",
         config = function ()
-            require("rose-pine").setup({
-                palette = {
-                    moon = {
-                        base = "#000000",
-                        overlay = "#1a1820",
-                    }
-                },
-                highlight_groups = {
-                    TelescopeBorder = { fg = "highlight_high", bg = "none" },
-                    TelescopeNormal = { bg = "none" },
-                    TelescopePromptNormal = { bg = "base" },
-                    TelescopeResultsNormal = { fg = "subtle", bg = "base" },
-                    TelescopeSelection = { fg = "text", bg = "base" },
-                    NormalFloat = { bg = "base" },
-                    FloatBorder = { fg = "highlight_high", bg = "none" },
+            ---@ type CatppuccinOptions catppuccin_options
+            local catppuccin_options = {
+                term_colors = false,
+                flavour = "mocha",
+                highlight_overrides = {
+                    ---@ type CtpHighlightOverrideFn mocha
+                    mocha = function (colors)
+                        return {
+                            Cursor = { fg = colors.crust, bg = colors.rosewater },
+                        }
+                    end
                 }
-            })
-            vim.cmd([[colorscheme rose-pine-moon]])
+            }
+
+            require("catppuccin").setup(catppuccin_options)
+
+            vim.cmd([[colorscheme catppuccin-mocha]])
         end
-    },
+    }
 }
 
 return colorschemes[1]
